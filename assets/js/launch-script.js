b80363c09e28e051574cec644f4d41c794e8bbcc
Jett Pavlica <jettpav@gmail.com>
2023-12-29 13:01:46 -0500

    fixed up launch script

--- assets/js/launch-script.js
+++ assets/js/launch-script.js
@@ -1,18 +1,19 @@
 const atomicStructureConfig = {
-			container: 'sketch-container',
-			particleInteractivity: {
-				protons: true,
-				electrons: true,
-				nuetrons: true,
-				shells: true,
-			},
-			atomData: {
-				protons: 10,
-				neutrons: 10,
-				shells: [2,4,4],
-				atomCard: true
-			},
-			colors: ["#4B0082", "#6C6377", "#FFD700"]
-		};
+  container: 'answer-visual-container45342',
+  particleInteractivity: {
+    protons: true,
+    electrons: true,
+    neutrons: true,
+    shells: true,
+  },
+  atomData: {protons: 10, neutrons: 10, shells: [2, 4, 4], atomCard: true},
+  atomColors: {
+    protonColor: '#4B0082',
+    neutronColor: '#6C6377',
+    electronColor: '#FFD700',
+    buttonFocusColor: '#DCEBFC'
+  }
+};
 
-		let removeAtomicStructureWidget = runAtomicStructureWidget(atomicStructureConfig);
+let removeAtomicStructureWidget =
+    runAtomicStructureWidget(atomicStructureConfig);
