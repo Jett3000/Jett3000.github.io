fb7299d5bb820848106201ebdc9a8c1dc5ef7fec
Jett Pavlica <jettpav@gmail.com>
2024-05-04 00:44:11 -0400

    bento homepage, bio, and space

---
+++ assets/js/lover.js
@@ -0,0 +1,4 @@
+// sets an event listener to tell site visitors that we love them too :)
+
+let heartButton = document.getElementById('heart-button');
+heartButton.addEventListener('click', ()=>{alert('I love you too!')});
