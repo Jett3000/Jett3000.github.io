c8408e2f49062a0c43853f86a96df8e703dc5d93
Jett Pavlica <jettpav@gmail.com>
2025-07-14 23:49:39 -0400

    purge for 2025 edition

--- assets/js/lover.js
+++
@@ -1,4 +0,0 @@
-// sets an event listener to tell site visitors that we love them too :)
-
-let heartButton = document.getElementById('heart-button');
-heartButton.addEventListener('click', ()=>{alert('I love you too!')});
